dbab6de4e6443bae20685b2fb7665f99fe67e738
João Pedro Martins Cruz <103667165+martins-joaopedro@users.noreply.github.com>
2024-06-25 09:37:47 -0300

    feat: starts api consuming

--- src/pages/feed/index.jsx
+++ src/pages/feed/index.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Post } from '../../components/post'
 
 export const Feed = () => {
+  
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    
+    getPosts()
+  }, [])
+
+  const getPosts = async () => {
+    const data = await fetchPosts();
+    setPosts(data)
+  }
+
+  const fetchPosts = async () => {
+    const data = await fetch("https://blogapi-mqr3.onrender.com/post")
+    const json = await data.json()
+    console.log(json)
+    return json
+  }
+
+  const sendPost = () => {
+    const { value } = document.querySelector("#post-input")
+    
+    let body = {
+        authorId: "joao",
+        text: value
+    }
+
+    fetch("https://blogapi-mqr3.onrender.com/post", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body)
+    })
+  }
+
   return (
     <div>
-        Feed
-        <Post />
+      <div>
+
+        <input id="post-input" /> 
+        <button onClick={() => sendPost()} />
+      </div>
+
+        {
+          posts?.map(data => (<Post data={data} />))
+        }
     </div>
   )
 }
